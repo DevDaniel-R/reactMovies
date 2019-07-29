8ce5db0390c84e0357430b8d5812e206f4e8793b
Daniel Rodriguez <drodriguez661@gmail.com>
2019-07-29 09:53:37 -0500

    fetching data to image banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage' ;
 import SearchBar from '../elements/SearchBar/SearchBar' ;
 import FourColGrid from '../elements/FourColGrid/FourColGrid' ;
@@ -20,28 +20,65 @@ class Home extends Component {
 
   componentDidMount(){
     this.setState({ loading: true });
-    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-    this.fetchItems(endPoint);
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
+  }
+
+  searchItems = (searchTerm) => {
+    let endpoint = '';
+    this.setState({
+      movies: [],
+      loading: true,
+      searchTerm
+    })
+
+    if (searchTerm === '') {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    }else {
+      endpoint =`${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
   }
 
   loadMoreItems = () => {
-    let endPoint = '';
+    let endpoint = '';
     this.setState({ loading: true });
 
     if (this.state.searchTerm === '') {
-      endPoint = `${API_URL} movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+      endpoint = `${API_URL} movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
     } else {
-      endPoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
     }
     this.fetchItems(endpoint);
-  
   }
 
+  fetchItems = (endpoint) => {
+    fetch(endpoint) 
+    .then(result => result.json())
+    .then(result => {
+      this.setState({
+        movies: [...this.state.movies, ...result.results],
+        heroImage: this.state.heroImage || result.results[3],
+        loading: false,
+        currrentPage: result.page,
+        totalPages: result.total_pages
+      })
+    })
+    .catch(error => console.error('Error:', error))
+  }
+  
     render() {
       return (
         <div className="rmdb-home">
-          <HeroImage />
-          <SearchBar />
+          {this.state.heroImage ?
+            <div>
+              <HeroImage 
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+              title={this.state.heroImage.original_title}
+              text={this.state.heroImage.overview}
+            />
+            <SearchBar callback={this.searchItems} />
+          </div> : null }
           <FourColGrid />
           <Spinner />
           <LoadMoreBtn />
